dcb77524b5e4524a536d861611a83decc131e8c6
Raghav <agu94.raghav@gmail.com>
2020-06-16 04:43:31 +0530

    Created Game Class

---
+++ src/strategize/game.py
@@ -0,0 +1,26 @@
+class Game:
+
+    '''
+    Game currently represents a normal form game
+    
+    ## Artificial Limits
+    
+    - player_set = 2
+    - action_set = 2 per player
+
+    Artifical limts are imposed for easy representation
+    Limits will be lifted in future releases
+    '''
+
+    def __init__(self, player_set, action_set, utility_set):
+        num_players = player_set.shape[0]
+        print('Initializing a {} player game'.format(num_players))
+        self.N = player_set
+        self.A = action_set
+        self.u = utility_set
+    
+    def __repr__(self):
+        return '''Game (N, A, u) where 
+    N = {}
+    A = {}
+    u = {}'''.format(self.N, self.A, self.u)
